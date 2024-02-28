ae2fe7514468be5be613c4f88c2ade26d7020438
David <david.toledo@seoprohub.com>
2024-02-28 05:11:06 +0500

    first commit

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,6 +1,7 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-
+// import { lusitana } from '@/app/ui/fonts';
+// ${lusitana.className}
 export default function AcmeLogo() {
   return (
     <div
